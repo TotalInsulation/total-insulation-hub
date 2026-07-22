9ab9a8b6e587efe2a965cacd56a747f02dcdbdf1
TotalInsulation <jwallace@totalinsulation.com.au>
2026-07-22 17:25:35 +1000

    Delete src directory

--- src/lib/navigation.ts
+++
@@ -1,8 +0,0 @@
-export type NavigateDetail = {
-  tab: 'home' | 'onsite' | 'business' | 'crew' | 'team' | 'more';
-  subTab?: string;
-};
-
-export function navigateTo(tab: NavigateDetail['tab'], subTab?: string) {
-  window.dispatchEvent(new CustomEvent<NavigateDetail>('ti-navigate', { detail: { tab, subTab } }));
-}
